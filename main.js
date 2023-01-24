012041d25e76d83a2d519d6393701706ef3041e7
Edvinas1982 <79442331+Edvinas1982@users.noreply.github.com>
2023-01-24 17:18:05 +0200

    task_done

--- main.js
+++ main.js
@@ -10,37 +10,53 @@ var volumesTbody = document.querySelector('#volumes tbody');
 var second_button = document.getElementById('second_button');
 var  volumes = [];
 var third_button = document.getElementById('third_button');
+var initialDataPressed = false;
+var findMaxPressed = false;
+
 
  // initial data button, take data from figures array, create table and put those data in table
  first_button.addEventListener('click', function () {
-    for (var i = 0; i < figures.length; i++) {
-        var tr = document.createElement('tr');
-        var lenghtTd = document.createElement('td');
-        lenghtTd.innerHTML = figures[i].lenght;
-        var widthTd = document.createElement('td');
-        widthTd.innerHTML = figures[i].width;
-        var heightTd = document.createElement('td');
-        heightTd.innerHTML = figures[i].height;
-        tr.appendChild(lenghtTd);
-        tr.appendChild(widthTd);
-        tr.appendChild(heightTd);
-        dataTbody.appendChild(tr);
-        var volume = figures[i].lenght * figures[i].width * figures[i].height; 
-        volumes.push(volume);
+    if (!initialDataPressed) {
+        for (var i = 0; i < figures.length; i++) {
+            var tr = document.createElement('tr');
+            var lenghtTd = document.createElement('td');
+            lenghtTd.innerHTML = figures[i].lenght;
+            var widthTd = document.createElement('td');
+            widthTd.innerHTML = figures[i].width;
+            var heightTd = document.createElement('td');
+            heightTd.innerHTML = figures[i].height;
+            tr.appendChild(lenghtTd);
+            tr.appendChild(widthTd);
+            tr.appendChild(heightTd);
+            dataTbody.appendChild(tr);
+            var volume = figures[i].lenght * figures[i].width * figures[i].height; 
+            volumes.push(volume);
+        }
     }
+    initialDataPressed = true;
 });
 // find MAX button, searching maximum hight of volume, create table and put all volumes in table, and adding "red" class to highest value 
 second_button.addEventListener('click', function () {
-    var max = Math.max(...volumes);
-    for (var i = 0; i < volumes.length; i++) {
-        var tr = document.createElement('tr');
-        var td = document.createElement('td');
-        if (max == volumes[i]) {
-            td.classList.add('red')
+    if (!findMaxPressed) {
+        var max = Math.max(...volumes);
+        for (var i = 0; i < volumes.length; i++) {
+            var tr = document.createElement('tr');
+            var td = document.createElement('td');
+            if (max == volumes[i]) {
+                td.classList.add('red')
+            }
+            td.innerHTML = volumes[i];
+            tr.appendChild(td);
+            volumesTbody.appendChild(tr);
         }
-        td.innerHTML = volumes[i];
-        tr.appendChild(td);
-        volumesTbody.appendChild(tr);
+        volumes = [];
     }
+    findMaxPressed = true;
 });
-// reset button
+// delete data from tables
+third_button.addEventListener('click', function () {
+    dataTbody.innerHTML = '';
+    volumesTbody.innerHTML = '';
+    initialDataPressed = false;
+    findMaxPressed = false;
+});
